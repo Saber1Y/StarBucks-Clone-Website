77f1377fae6a4f65d50734b75942ebab38312a81
ayodeji <ayodejiakintobi1@gmail.com>
2023-07-22 20:44:16 +0100

    footer updated

---
+++ js.js
@@ -0,0 +1,7 @@
+function togglemenu() {
+    if (mobileMenu.style.maxHeight == "0px") {
+        mobileMenu.style.maxHeight = "130px";
+    } else {
+        mobileMenu.style.maxHeight = "0px";
+    }
+}
