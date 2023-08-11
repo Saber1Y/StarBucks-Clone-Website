7009b3b6c3cee2cfb5185a9c07edbf2a24b30cb4
ayodeji <ayodejiakintobi1@gmail.com>
2023-08-12 00:45:23 +0100

    responsive nav-bar

--- js.js
+++ js.js
@@ -1,7 +1,14 @@
-function togglemenu() {
-    if (mobileMenu.style.maxHeight == "0px") {
-        mobileMenu.style.maxHeight = "130px";
-    } else {
-        mobileMenu.style.maxHeight = "0px";
-    }
+// function togglemenu() {
+//     if (mobileMenu.style.maxHeight == "0px") {
+//         mobileMenu.style.maxHeight = "130px";
+//     } else {
+//         mobileMenu.style.maxHeight = "0px";
+//     }
+// }
+
+
+function menuClick() {
+    const menuIcon = document.getElementById('menuIcon');
+    menuIcon.classList.toggle('bx-menu-alt-right');
+    menuIcon.classList.toggle('bx-x');
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html*"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1400px",
+      "2xl": "1536px",
+     },
+    extend: {
+      colors: {
+        darkBlack: "hsla(0, 0%, 0%, 1)",
+        lightGray: "hsla(0, 0%, 50%, 1)",
+        lightGreen: "hsla(162, 100%, 20%, 1)",
+      }
+    },
+  },
+  plugins: [],
+}
+
