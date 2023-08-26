c169b1774c81b8259d6bfcdaf68a6ca0c8675ed5
ayodeji <ayodejiakintobi1@gmail.com>
2023-08-26 13:58:28 +0100

    completed mobile res

--- js.js
+++ js.js
@@ -32,18 +32,31 @@ Arrows.forEach((arrow, index) => {
 //     itemDisplay();
 // }, 1000);
 
-const latteImageContainer = document.getElementById('img25');
-const latteTextContainer = document.getElementById('text25');
-
+document.addEventListener('DOMContentLoaded', function() {
+  showImage('img25');
+});
 
 function showImage(imageId) {
   const imageContainers = document.querySelectorAll('#image-result > div');
+  
   imageContainers.forEach(container => {
-    container.classList.add('hidden');
+    if (container.id === imageId) {
+      container.style.display = 'block';
+    } else {
+      container.style.display = 'none';
+    }
   });
-
-  const selectedContainer = document.getElementById(imageId);
-  if (selectedContainer) {
-    selectedContainer.classList.remove('hidden');
-  }
 }
+
+// window.addEventListener('scroll', function() {
+//   const stickySection = document.getElementById('sticky-section');
+//   const scrollPosition = window.scrollY;
+
+//   if (scrollPosition >= 20) {
+//     stickySection.classList.add('fixed', 'top-0');
+//     stickySection.style.width = `${stickySection.offsetWidth}px`;
+//   } else {
+//     stickySection.classList.remove('fixed', 'top-0');
+//     stickySection.style.width = 'auto';
+//   }
+// });
